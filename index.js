bf530888ea71557c11fb996cd5657847cfd03d82
paxasaval <paxasaval1003@gmail.com>
2023-03-23 21:41:36 -0500

    deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
-
+app.use(cors())
+app.use(express.static('build'))
 morgan.token('body',(req)=>(req.method==='POST'? JSON.stringify(req.body):''))
 
 app.use(morgan(':method :url :status - :response-time ms :body'))
