9a87d04565a2ec892ba358143baf872d3f81f567
paxasaval <paxasaval1003@gmail.com>
2023-03-23 21:20:24 -0500

    deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -93,7 +94,7 @@ app.delete('/api/persons/:id',(req,res)=>{
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
